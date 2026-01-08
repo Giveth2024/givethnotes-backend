90a6d8bfd6b72c8f7e2e9d3f93d30054b5ce10dc
Giveth2024 <givendarian@gmail.com>
2026-01-08 14:48:24 +0300

    Generated Auth Token and sent to backend. Authentication between frontend and backend was sucessful

--- app.js
+++ app.js
@@ -6,10 +6,16 @@ const database = require("./config/db");
 const app = express();
 
 // Middlewares
+const { clerkMiddleware, clerkClient, requireAuth, getAuth } = require('@clerk/express');
 const logger = require('./middleware/logger');
 const errorHandler = require('./middleware/error');
 const notFound = require('./middleware/notfound');
-app.use(cors());
+
+app.use(cors({
+    origin: 'http://localhost:3000', // your Next.js frontend
+    credentials: true,               // allow cookies
+  }));
+app.use(clerkMiddleware());
 app.use(express.json());
 app.set('trust proxy', true);
 app.use(logger);
@@ -24,14 +30,25 @@ app.get('/', (req, res) => {
   res.json({ message: 'GivethNotes API is running 🚀' });
 });
 
+// If user isn't authenticated, requireAuth() will redirect back to the homepage
+app.get('/protected', requireAuth(), async (req, res) => {
+  // Use `getAuth()` to get the user's `userId`
+  const { userId } = getAuth(req)
+
+  // Use Clerk's JS Backend SDK to get the user's User object
+  const user = await clerkClient.users.getUser(userId)
+
+  return res.json({ user })
+})
+
 // Career Routes Paths
-app.use('/api', careerPathsRoutes);
+app.use('/api', requireAuth(), careerPathsRoutes);
 
 // Journal Entry Routes Paths
-app.use('/api', journalEntryRoutes);
+app.use('/api', requireAuth(), journalEntryRoutes);
 
 // Entry Blocks Routes Paths
-app.use('/api', entryBlocksRoutes);
+app.use('/api', requireAuth(), entryBlocksRoutes);
 
 // 404 Middleware
 app.use(notFound);
